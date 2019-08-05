a4841d748033e71ffa90d9503d8477a429b0a19f
unknown <satyaraj@live.com>
2019-08-05 12:53:42 +0530

    added Index and other supporting files

---
+++ precache-manifest.ba18214688830f3b0dbc08488dac6b73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58511633f78acde126f97bcbd15b5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c69bb174f8d6df8cd6",
+    "url": "/static/css/main.044b6152.chunk.css"
+  },
+  {
+    "revision": "65c86e1d36e3f7b4ad7c",
+    "url": "/static/js/2.8464044c.chunk.js"
+  },
+  {
+    "revision": "a6c69bb174f8d6df8cd6",
+    "url": "/static/js/main.97b1ee8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
